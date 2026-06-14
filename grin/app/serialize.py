650900d648fe11dfc6fb8dddd297e3df69f5dc5b
parsazolfghar <parsazolfaghar@yahoo.com>
2026-06-13 22:25:05 -0700

    feat: app package skeleton, [app] extra, JSON serialization helper

---
+++ grin/app/serialize.py
@@ -0,0 +1,12 @@
+"""Turn engine dataclasses into JSON-serializable structures for the JS bridge."""
+import dataclasses
+
+
+def to_jsonable(obj):
+    if dataclasses.is_dataclass(obj) and not isinstance(obj, type):
+        return {k: to_jsonable(v) for k, v in dataclasses.asdict(obj).items()}
+    if isinstance(obj, dict):
+        return {k: to_jsonable(v) for k, v in obj.items()}
+    if isinstance(obj, (list, tuple)):
+        return [to_jsonable(v) for v in obj]
+    return obj
